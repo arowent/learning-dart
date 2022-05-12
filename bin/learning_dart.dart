45c3b90bdbe62070f95f9eed28705235ade580d8
arowent <arowent@mail.ru>
2022-05-12 19:22:46 +0300

    update

--- bin/learning_dart.dart
+++ bin/learning_dart.dart
@@ -1,5 +1,8 @@
 // import 'package:learning_dart/learning_dart.dart' as learning_dart;
-import 'package:learning_dart/trading/momentum.dart' as momentum;
+// ignore: unused_local_variable
+// import 'dart:svg';
+
+import "package:learning_dart/trading/momentum.dart" as momentum;
 
 class Person {
   String name = "undefined";
@@ -12,9 +15,39 @@ class Person {
   }
 }
 
-void main(List<String> arguments) {
-  Person tim = Person("Tim", 24);
-  Person sam = Person("", 25);
-  tim.info();
-  sam.info();
+void example(List<int> numbers) {
+  for (int i in numbers) {
+    if (i % 2 == 0) {
+      print('Number $i - четное число');
+    } else {
+      print('Number $i - нечетное число');
+    }
+  }
+}
+
+void main() {
+  // Person tim = Person("Tim", 24);
+  // Person sam = Person("", 25);
+  // tim.info();
+  // sam.info();
+  var numbers = [2, 7, 3, 5, 8, 13];
+  int? numberOne;
+  int numberTwo = numberOne ?? 0;
+
+  switch (numbers.length) {
+    case 7:
+      print("Длина списка равна 7");
+      break;
+    case 1:
+      print('ты говно');
+      break;
+
+    default:
+      print('Сосать!');
+  }
+
+  int x = 3;
+  int y = 2;
+  int z = x < y ? (x + y) : (x - y);
+  print(z);
 }
